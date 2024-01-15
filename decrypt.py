47d9cedf988d51a5667163565050ff553cd47e19
Roiyan Zain <zayn@Roiyans-MacBook-Pro.local>
2024-01-15 09:26:11 +0700

    update 2.1.3

--- decrypt.py
+++ decrypt.py
@@ -7,7 +7,9 @@ from io import BytesIO
 # Fungsi untuk mendownload gambar stego ke dalam bentuk 'JPG'
 def get_image_download_link(img, filename, text):
     buffered = BytesIO()
-    img.save(buffered, format='JPEG')  # Use 'JPEG' as the format
+    if img.mode in ('RGBA', 'P'):  # Jika gambar memiliki alpha channel
+        img = img.convert('RGB')  # Konversi ke 'RGB'
+    img.save(buffered, format='JPEG')  # Gunakan 'JPEG' sebagai format penyimpanan
     img_str = base64.b64encode(buffered.getvalue()).decode()
     href = f'<a href="data:image/jpeg;base64,{img_str}" download="{filename}">{text}</a>'  # Use 'image/jpeg' as the MIME type
     return href
